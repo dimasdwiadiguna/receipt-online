64f38a5008244baf75be355b8dfe6639a1d26190
Dimas <dimasdwiadiguna@gmail.com>
2022-07-26 16:39:54 +0700

    test new layout with graphic

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useParams, useLocation } from "react-router-dom";
+import Logo from "../assets/gotriksvg2.svg";
+import "./Receipt.css";
 
 function useQuery() {
   const { search } = useLocation();
@@ -9,13 +11,16 @@ function useQuery() {
 const Receipt = ({ orderId, nama, berat, diskon, totalBiaya, status }) => {
   return (
     <>
-      <div>Order {orderId}</div>
-      <div>Nama : {nama}</div>
+      <img src={Logo} width="200px" />
+      <p>Negla Kencana Residence</p>
+      <hr />
+      <h1>{orderId}</h1>
+      <h2>{nama}</h2>
       <div>Berat : {berat} kg</div>
-      <div>---------------------</div>
       <div>Diskon : Rp.{diskon}</div>
       <div>Total : Rp.{totalBiaya}</div>
-      <div>Status : {status}</div>
+      <hr />
+      <p>Terima kasih telah menggunakan jasa goTrik setrika kiloan!</p>
     </>
   );
 };
