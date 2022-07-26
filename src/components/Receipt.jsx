369b5021568ef4a7c2939c58b1747f43f15895a1
Dimas <dimasdwiadiguna@gmail.com>
2022-07-26 18:01:47 +0700

    add more data

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -8,12 +8,20 @@ function useQuery() {
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
-const Receipt = ({ orderId, nama, berat, diskon, totalBiaya, status }) => {
+const Receipt = ({
+  orderId,
+  nama,
+  tglMasuk,
+  tglKeluar,
+  layanan,
+  berat,
+  diskon,
+  totalBiaya,
+}) => {
   return (
     <>
-      <img src={Logo} width="150px" />
-      <p>Negla Kencana Residence Blok C No 3</p>
-      <p>Ujung Berung, Bandung</p>
+      <img src={Logo} width="100px" />
+      <p>goTrik Setrika Kiloan</p>
       <p>WA : 0821-2775-3082</p>
       <p>-----------------------------</p>
       <p> </p>
@@ -24,13 +32,19 @@ const Receipt = ({ orderId, nama, berat, diskon, totalBiaya, status }) => {
       <p>-----------------------------</p>
       <div className="center">
         <p>{nama}</p>
+        <p>
+          Masuk : {tglMasuk} - {layanan}
+        </p>
+        <p>Perkiraan selesai : {tglKeluar}</p>
         <p>Berat : {berat} kg</p>
-        <p>Diskon : Rp.{diskon}</p>
+        <p>(Diskon : Rp.{diskon})</p>
         <p>Total : Rp.{totalBiaya}</p>
       </div>
-      <p>-----------------------------</p>
+      <p>--**--</p>
       <p>Terima kasih. Semoga berkah.</p>
-      <p>goTrik Setrika Kiloan</p>
+      <p>-----------------------------</p>
+      <p>Negla Kencana Residence Blok C No 3</p>
+      <p>Ujung Berung, Bandung</p>
     </>
   );
 };
