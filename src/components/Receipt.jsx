85e284c77021ee8540e1371261103b875df6113a
Dimas <dimasdwiadiguna@gmail.com>
2022-10-04 05:59:17 +0700

    relocate gotrik. some info changed

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -23,8 +23,8 @@ const Receipt = ({
     <>
       <img src={Logo} width="100px" />
       <p>goTrik Setrika Kiloan</p>
-      <p>WA : 0821-2775-3082</p>
-      <p>-----------------------------</p>
+      <p>WA : 0821-1285-6539</p>
+      <p>=============================</p>
       <p> </p>
       <div className="border">
         <p className="large">
@@ -32,7 +32,7 @@ const Receipt = ({
         </p>
       </div>
       <p> </p>
-      <p>-----------------------------</p>
+      <p>=============================</p>
       <div className="center">
         <p>{nama}</p>
         <p>Masuk : {tglMasuk}</p>
@@ -41,13 +41,14 @@ const Receipt = ({
         <p>-----------------------------</p>
         <p>(Ongkos kirim : +Rp.{ongkir})</p>
         <p>(Diskon : -Rp.{diskon})</p>
+        <p>-----------------------------</p>
         <p>TOTAL : Rp.{totalBiaya}</p>
       </div>
       <p> . </p>
       <p>Terima kasih. Semoga berkah.</p>
       <p>--**--</p>
-      <p>Negla Kencana Residence Blok C No 3</p>
-      <p>Ujung Berung, Bandung</p>
+      <p>Jl Bola Basket No 26 (Perluasan)</p>
+      <p>Arcamanik, Bandung</p>
     </>
   );
 };
